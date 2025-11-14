75d79beed65ad833814c5c812c923eb17cf50e64
Marharyta <nasty22kot@gmail.com>
2025-11-14 13:43:41 +0200

    Changed vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname), // або './' якщо запускаєш з кореня client
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
 });
